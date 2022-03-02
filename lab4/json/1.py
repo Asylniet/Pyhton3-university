87c7beacd2bd714f6b0148d4f3f445899cda6da6
Asylniet <zasylniet@mail.ru>
2022-03-02 22:15:37 +0600

    Json task update

--- lab4/json/1.py
+++ lab4/json/1.py
@@ -6,7 +6,7 @@ with open('sample-data.json', 'r') as f:
 table = dict()
 database = list()
 a = json.loads(data)
-n = ["dn", "speed", "delay", "id"]
+n = input().split()
 for j in a["imdata"] :
     for i in n :
         table[i] = j["l1PhysIf"]["attributes"][i]
@@ -14,7 +14,27 @@ for j in a["imdata"] :
 
 print("Interface Status")
 print("==========================================================================")
-print ("{:<45} {:<10} {:<8} {:<10}".format(n[0], n[1], n[2], n[3]))
-print("------------------------------------------    --------   -------  --------")
-for k in database:
-    print ("{:<45} {:<10} {:<8} {:<10}".format(k[n[0]], k[n[1]], k[n[2]], k[n[3]]))
+
+for i in n :
+    x = len(database[0][i])
+    print(f"{i:<{x}}", end = "\t")
+
+print()
+
+for i in n :
+    if len(i) < len(database[0][i]) :
+        for j in range(len(database[0][i])) :
+            print("-", end = "")
+        print("", end = "\t")
+    else :
+        for j in range(len(i)) :
+            print("-", end = "")
+        print("", end = "\t")
+
+print()
+
+for i in database:
+    for j in n :
+        print(i[j], end = "\t")
+
+    print()
